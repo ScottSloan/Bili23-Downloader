45d34284ba7f9653f2ad55b4b8dd4385cbd21f9f
ScottSloan <90490067+ScottSloan@users.noreply.github.com>
2022-03-20 05:43:50 +0000

    Update __init__.py

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,6 +1,1 @@
-import ctypes
-import platform
-
-machine_platform = platform.platform()
-if machine_platform.startswith("Windows-10") or machine_platform.startswith("Windows-11"):
-    ctypes.windll.shcore.SetProcessDpiAwareness(2)
+# init
