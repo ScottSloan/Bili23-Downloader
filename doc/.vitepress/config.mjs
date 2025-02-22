247da3e726ee27569e99763037c6789673c42660
ScottSloan <world1019@sina.com>
2025-02-22 23:48:12 +0800

    更新文档

--- doc/.vitepress/config.mjs
+++ doc/.vitepress/config.mjs
@@ -3,42 +3,102 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Bili23 Downloader",
-  description: "一个B 站视频下载工具",
+  description: "一个 B 站视频下载工具",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: '首页', link: '/' },
-      { text: '文档', link: '/doc' },
-      { text: '博客', link: 'https://www.scott-sloan.cn'}
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: '文档',
+        link: '/doc/intro'
+      },
+      {
+        text: '博客',
+        link: 'https://www.scott-sloan.cn'
+      }
     ],
 
     sidebar: [
       {
         text: '简介',
         items: [
-          { text: 'Bili23 Downloader 简介', link: '/doc/intro-bili23'}
+          {
+            text: 'Bili23 Downloader 简介',
+            link: '/doc/intro'
+          }
         ]
       },
       {
         text: '安装',
+        collapsed: true,
         items: [
-          { text: '安装主程序', link: '/doc/install/main' },
-          { text: '安装 FFmpeg', link: '/doc/install/ffmpeg' }
+          {
+            text: '安装主程序',
+            link: '/doc/install/main'
+          },
+          {
+            text: '安装 FFmpeg',
+            link: '/doc/install/ffmpeg'
+          }
         ]
       },
       {
         text: '使用',
+        collapsed: true,
         items: [
-          { text: '基础使用', link: '/doc/use/basic'},
-          { text: '支持的链接', link: '/doc/use/url'}
+          {
+            text: '基础使用',
+            link: '/doc/use/basic'
+          },
+          {
+            text: '支持的链接',
+            link: '/doc/use/url'
+          }
         ]
       },
       {
         text: '更新记录',
+        collapsed: true,
         items: [
-          { text: '1.55.0', link: '/doc/history/log_1550'},
-          { text: '1.54.0', link: '/doc/history/log_1540'},
-          { text: '1.53.0', link: '/doc/history/log_1530'}
+          {
+            text: '1.50',
+            collapsed: true,
+            items: [
+              {
+                text: '1.55.0',
+                link: '/doc/history/log_1550'
+              },
+              {
+                text: '1.54.0',
+                link: '/doc/history/log_1540'
+              },
+              {
+                text: '1.53.0',
+                link: '/doc/history/log_1530'
+              },
+              {
+                text: '1.52.0',
+                link: '/doc/history/log_1520'
+              },
+              {
+                text: '1.51.0',
+                link: '/doc/history/log_1510'
+              },
+              {
+                text: '1.50.0',
+                link: '/doc/history/log_1500'
+              }
+            ]
+          },
+          {
+            text: '1.40',
+            collapsed: true,
+            items: [
+            ]
+          }
         ]
       }
     ],
