d2d2816f8753dca0f0d7328eac2053478f25b37e
ScottSloan <world1019@sina.com>
2025-03-07 22:39:42 +0800

    ✨ feat: 自定义下载文件名支持显示预览

--- src/gui/dialog/file_name.py
+++ src/gui/dialog/file_name.py
@@ -1,4 +1,5 @@
 import wx
+from datetime import datetime
 
 from utils.common.map import file_name_fields_map, get_mapping_key_by_value
 
@@ -17,7 +18,7 @@ class CustomFileNameDialog(wx.Dialog):
     def init_UI(self):
         available_lab = wx.StaticText(self, -1, "可用字段")
         self.available_choice = wx.Choice(self, -1, choices = list(file_name_fields_map.values()))
-        self.add_btn = wx.Button(self, -1, "添加字段")
+        self.add_btn = wx.Button(self, -1, "添加字段", size = self.FromDIP((80, 28)))
 
         available_hbox = wx.BoxSizer(wx.HORIZONTAL)
         available_hbox.Add(available_lab, 0, wx.ALL | wx.ALIGN_CENTER, 10)
@@ -52,7 +53,25 @@ class CustomFileNameDialog(wx.Dialog):
     def Bind_EVT(self):
         self.add_btn.Bind(wx.EVT_BUTTON, self.onAddFieldEVT)
 
+    def show_preview_file_name(self):
+        date_field = datetime.now().strftime("%Y-%m-%d")
+        time_field = datetime.now().strftime("%H:%M:%S")
+        datetime_field = f"{date_field} {time_field}"
+        timestamp_field = str(int(datetime.now().timestamp()))
+        index_field = "1"
+        index_with_zero_field = "01"
+        title_field = "《孤独摇滚》第1话 孤独的转机"
+        bvid_field = "BV1yW4y1j7Ft"
+        cid_field = "875212290"
+
+        template = str(self.template_box.GetValue())
+        preview = template.format(date = date_field, time = time_field, datetime = datetime_field, timestamp = timestamp_field, index = index_field, index_with_zero = index_with_zero_field, title = title_field, bvid = bvid_field, cid = cid_field)
+
+        self.preview_lab.SetLabel(f"预览：{preview}")
+
     def onAddFieldEVT(self, event):
         field_name = get_mapping_key_by_value(file_name_fields_map, self.available_choice.GetStringSelection())
 
         self.template_box.AppendText("{" + field_name + "}")
+
+        self.show_preview_file_name()
