7bd2d80b999a02994ed5056458e2831f4ca156e3
ScottSloan <world1019@sina.com>
2025-03-07 23:24:52 +0800

    ✨ feat: 增加自定义字段格式检查

--- src/gui/dialog/file_name.py
+++ src/gui/dialog/file_name.py
@@ -13,6 +13,8 @@ class CustomFileNameDialog(wx.Dialog):
 
         self.Bind_EVT()
 
+        self.init_utils()
+
         self.CenterOnParent()
 
     def init_UI(self):
@@ -32,6 +34,15 @@ class CustomFileNameDialog(wx.Dialog):
         template_hbox.Add(template_lab, 0, wx.ALL & (~wx.TOP) | wx.ALIGN_CENTER, 10)
         template_hbox.Add(self.template_box, 0, wx.ALL & (~wx.TOP) & (~wx.LEFT) | wx.ALIGN_CENTER, 10)
 
+        self.error_icon = wx.StaticBitmap(self, -1, wx.ArtProvider().GetBitmap(wx.ART_ERROR, size = self.FromDIP((16, 16))), size = self.FromDIP((16, 16)))
+        self.error_lab = wx.StaticText(self, -1)
+        self.error_lab.SetForegroundColour(wx.Colour(250, 42, 45))
+
+        error_hbox = wx.BoxSizer(wx.HORIZONTAL)
+        error_hbox.AddSpacer(template_lab.GetSize()[0])
+        error_hbox.Add(self.error_icon, 0, wx.ALL & (~wx.TOP) & (~wx.BOTTOM), 10)
+        error_hbox.Add(self.error_lab, 0, wx.ALL & (~wx.TOP) & (~wx.BOTTOM) & (~wx.LEFT), 10)
+
         self.preview_lab = wx.StaticText(self, -1, "预览：")
 
         self.ok_btn = wx.Button(self, wx.ID_OK, "确定", size = self.FromDIP((80, 30)))
@@ -45,6 +56,7 @@ class CustomFileNameDialog(wx.Dialog):
         vbox = wx.BoxSizer(wx.VERTICAL)
         vbox.Add(available_hbox, 0, wx.EXPAND)
         vbox.Add(template_hbox, 0, wx.EXPAND)
+        vbox.Add(error_hbox, 0, wx.EXPAND)
         vbox.Add(self.preview_lab, 0, wx.ALL, 10)
         vbox.Add(bottom_hbox, 0, wx.EXPAND)
 
@@ -52,6 +64,11 @@ class CustomFileNameDialog(wx.Dialog):
 
     def Bind_EVT(self):
         self.add_btn.Bind(wx.EVT_BUTTON, self.onAddFieldEVT)
+        self.template_box.Bind(wx.EVT_TEXT, self.onTextEVT)
+
+    def init_utils(self):
+        self.error_icon.Show(False)
+        self.Layout()
 
     def show_preview_file_name(self):
         date_field = datetime.now().strftime("%Y-%m-%d")
@@ -69,9 +86,40 @@ class CustomFileNameDialog(wx.Dialog):
 
         self.preview_lab.SetLabel(f"预览：{preview}")
 
+    def onTextEVT(self, event):
+        try:
+            self.show_preview_file_name()
+
+            self.check_pass()
+
+        except Exception as e:
+            self.syntax_error(str(e))
+
+        event.Skip()
+
     def onAddFieldEVT(self, event):
         field_name = get_mapping_key_by_value(file_name_fields_map, self.available_choice.GetStringSelection())
 
         self.template_box.AppendText("{" + field_name + "}")
 
-        self.show_preview_file_name()
+    def check_pass(self):
+        self.error_icon.Show(False)
+        self.error_lab.SetLabel("")
+
+        self.ok_btn.Enable(True)
+
+    def syntax_error(self, e: str):
+        self.error_icon.Show(True)
+
+        if e.startswith("Single") or e.startswith("expected"):
+            self.error_lab.SetLabel("字段名必须以 {} 包裹")
+        elif e.startswith("Replacement"):
+            self.error_lab.SetLabel("字段名称错误")
+        else:
+            self.error_lab.SetLabel("字段名称错误")
+
+        self.preview_lab.SetLabel("预览：-")
+
+        self.ok_btn.Enable(False)
+
+        self.Layout()
