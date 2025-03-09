cc2e39ae969b8149ae4b52b514b59230f099abfd
ScottSloan <world1019@sina.com>
2025-03-09 16:42:22 +0800

    ✨ feat: 支持下载多个文件

--- src/utils/module/downloader_v2.py
+++ src/utils/module/downloader_v2.py
@@ -4,6 +4,7 @@ import threading
 from concurrent.futures import ThreadPoolExecutor
 
 from utils.common.data_type import DownloadTaskInfo, RangeDownloadInfo, DownloaderInfo
+from utils.common.thread import Thread
 from utils.tool_v2 import DownloadFileTool, RequestTool
 from utils.config import Config
 
@@ -11,6 +12,8 @@ class Downloader:
     def __init__(self, task_info: DownloadTaskInfo, file_tool: DownloadFileTool):
         self.task_info = task_info
         self.file_tool = file_tool
+
+        self.init_utils()
 
     def init_utils(self):
         self.lock = threading.Lock()
@@ -29,43 +32,71 @@ class Downloader:
 
     def start_download(self):
         def get_downloader_info():
-            downloader_info = self.downloader_info[:1]
+            downloader_info = DownloaderInfo()
+            downloader_info.load_from_dict(self.downloader_info[:1][0])
 
             return downloader_info
         
-        def get_range_info(index: int, file_name: str, url: str, range: list):
-            range_info = RangeDownloadInfo()
-            range_info.index = index
-            range_info.file_path = os.path.join(Config.Download.path, file_name)
-            range_info.url = url
-            range_info.range = range
+        def get_ranges():
+            def get_total_size():
+                pass
 
-            return range_info
-        
-        downloader_info = DownloaderInfo()
-        downloader_info.load_from_dict(get_downloader_info())
+            if self.task_info.total_size:
+                # 断点续传
+                pass
+            else:
+                self.current_file_size = self.get_file_size(url, file_path)
+                return self.generate_ranges(self.current_file_size)
 
-        ranges = self.generate_ranges()
+        def worker():
+            def get_range_info(index: int, file_path: str, url: str, range: list):
+                range_info = RangeDownloadInfo()
+                range_info.index = index
+                range_info.file_path = file_path
+                range_info.url = url
+                range_info.range = range
 
-        with ThreadPoolExecutor() as executor:
-            for index, range in enumerate(ranges):
-                self.progress_info[index] = range
-                range_info = get_range_info(index, downloader_info.file_name)
+                return range_info
 
-                executor.submit(self.download_range, range_info)
+            progress_thread = Thread(target = self.progress_tracker)
+            progress_thread.start()
+
+            with ThreadPoolExecutor() as executor:
+                for index, range in enumerate(ranges):
+                    self.progress_info[index] = range
+                    range_info = get_range_info(index, file_path, url, range)
+
+                    executor.submit(self.download_range, range_info)
+            
+            progress_thread.join()
+
+        downloader_info = get_downloader_info()
+
+        url = downloader_info.url_list[:1][0]
+        file_path = os.path.join(Config.Download.path, downloader_info.file_name)
+
+        ranges = get_ranges()
+
+        worker()
 
     def download_range(self, info: RangeDownloadInfo):
-        with open(info.file_path, "r+b") as f:
-            f.seek(range[0])
+        try:
+            with open(info.file_path, "r+b") as f:
+                f.seek(info.range[0])
 
-            with RequestTool.request_get(info.url, headers = RequestTool.get_headers(referer_url = self.task_info.referer_url, sessdata = Config.User.SESSDATA, range = range), stream = True) as req:
-                for chunk in req.iter_content(chunk_size = 1024):
-                    if chunk:
-                        with self.lock:
-                            f.write(chunk)
+                with RequestTool.request_get(info.url, headers = RequestTool.get_headers(referer_url = self.task_info.referer_url, sessdata = Config.User.SESSDATA, range = info.range), stream = True) as req:
+                    for chunk in req.iter_content(chunk_size = 1024):
+                        if chunk:
+                            with self.lock:
+                                f.write(chunk)
 
-                            self.downloaded_size += len(chunk)
-                            self.progress_info[info.index][0] += len(chunk)
+                                _chunk_size = len(chunk)
+
+                                self.current_downloaded_size += _chunk_size
+                                self.total_downloaded_size += _chunk_size
+                                self.progress_info[info.index][0] += _chunk_size
+        except Exception as e:
+            print(e)
 
     def get_file_size(self, url: str, file_path: str):
         def create_local_file():
@@ -74,10 +105,14 @@ class Downloader:
                     f.seek(file_size - 1)
                     f.write(b"\0")
 
-        req = RequestTool.request_head(url)
-        file_size = int(req.headers.get("Content-Length"))
+        req = RequestTool.request_head(url, RequestTool.get_headers(referer_url = self.task_info.referer_url))
 
-        create_local_file()
+        if "Content-Length" in req.headers:
+            file_size = int(req.headers.get("Content-Length"))
+
+            create_local_file()
+        else:
+            print("Not")
 
         return file_size
     
@@ -95,6 +130,14 @@ class Downloader:
         return ranges
 
     def progress_tracker(self):
+        def download_finish():
+            self.downloader_info = self.downloader_info[1:]
+
+            self.current_downloaded_size = 0
+
+            if self.downloader_info:
+                Thread(target = self.start_download).start()
+
         while self.current_downloaded_size < self.current_file_size:
             temp_downloaded_size = self.current_downloaded_size
 
@@ -105,4 +148,6 @@ class Downloader:
                 current_progress = (self.current_downloaded_size / self.current_file_size) * 100
                 total_progress = (self.total_downloaded_size / self.total_file_size) * 100
 
-            print(f"Progress: {current_progress:.2f}%, Speed: {speed:.2f}MB/s")
+            print(f"Current Progress: {current_progress:.2f}%, Total Progress: {total_progress:.2f}, Speed: {speed:.2f}MB/s")
+        
+        download_finish()
