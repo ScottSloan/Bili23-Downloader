b4b6a4940e895e82ab7c9c7a927b40aa3261181c
Ivan Hanloth <ivan@hanloth.com>
2025-09-02 22:57:34 +0800

    doc: 新增文档图片预览功能

--- doc/.vitepress/theme/index.js
+++ doc/.vitepress/theme/index.js
@@ -2,6 +2,10 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
 
 /** @type {import('vitepress').Theme} */
 export default {
@@ -12,6 +16,15 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
+    DefaultTheme.enhanceApp(ctx);
+    // Register global components, if you don't want to use it, you don't need to add it
+    ctx.app.component('vImageViewer', vImageViewer);
     // ...
+  },
+  setup() {
+    // Get route
+    const route = useRoute();
+    // Using
+    imageViewer(route);
   }
 }
