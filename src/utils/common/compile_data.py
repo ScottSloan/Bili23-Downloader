f6101a51d7e2ccbd6281d47b0fefed4e36bd3aed
ScottSloan <world1019@sina.com>
2025-11-17 23:05:31 +0800

    ✨ feat: workflow 增加 loader 编译步骤，以及 release 发布

--- src/utils/common/compile_data.py
+++ src/utils/common/compile_data.py
@@ -1,5 +1,6 @@
-date = "2025-10-21 12:25:35"
-commit = "5e7b2fed849a81f272403b53a9b7d4ddb0ae6d29"
-beta_ver = 1
-beta_flag = False
-compile = False
+{
+    "ver_major": 1,
+    "ver_minor": 70,
+    "ver_patch": 2,
+    "ver_build": 0
+}
