517fe73c4e4b2793f7d2b4442d914b6e1ec24f85
ScottSloan <world1019@sina.com>
2026-05-18 13:25:36 +0800

    ♻️ refactor: 重构二维码生成逻辑，去除 PIL 依赖

--- src/util/auth/qrcode.py
+++ src/util/auth/qrcode.py
@@ -1,11 +1,12 @@
 from PySide6.QtCore import QObject, Signal, QSize, Qt, QTimer
-from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QImage, QPainter, QPixmap
 
 from ..network.request import NetworkRequestWorker
 from ..common.enum import QRCodeScanStatus
 from ..thread.async_ import AsyncTask
 from .base import AuthBase
 
+from qrcode import QRCode as QRCodeGenerator
 from urllib.parse import urlencode
 
 class QRCode(AuthBase, QObject):
@@ -42,10 +43,42 @@ class QRCode(AuthBase, QObject):
 
         super().on_error(message)
 
+    def _build_qrcode_pixmap(self, data: str) -> QPixmap:
+        # 生成 QR 码矩阵
+        qr_code = QRCodeGenerator(border = 4)
+        qr_code.add_data(data)
+        qr_code.make(fit=True)
+
+        matrix = qr_code.get_matrix()
+        module_count = len(matrix)
+        box_size = max(1, 160 // module_count)
+        image_size = module_count * box_size
+
+        image = QImage(image_size, image_size, QImage.Format.Format_ARGB32)
+        image.fill(Qt.GlobalColor.white)
+
+        painter = QPainter(image)
+        try:
+            painter.setPen(Qt.PenStyle.NoPen)
+            painter.setBrush(Qt.GlobalColor.black)
+
+            for row_index, row in enumerate(matrix):
+                y = row_index * box_size
+
+                for column_index, is_dark in enumerate(row):
+                    if is_dark:
+                        painter.drawRect(column_index * box_size, y, box_size, box_size)
+        finally:
+            painter.end()
+
+        return QPixmap.fromImage(image).scaled(
+            QSize(160, 160),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation,
+        )
+
     def generate(self):
         def on_success(response: dict):
-            from qrcode import make as make_qrcode
-            
             if self._cleaned_up:
                 return
 
@@ -54,12 +87,7 @@ class QRCode(AuthBase, QObject):
             self.qrcode_url = response["data"]["url"]
             self.qrcode_key = response["data"]["qrcode_key"]
 
-            qrcode_image = make_qrcode(self.qrcode_url)
-
-            pixmap = QPixmap.fromImage(qrcode_image.toqimage())
-            pixmap = pixmap.scaled(QSize(160, 160), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
-
-            self.qrcode_generated.emit(pixmap)
+            self.qrcode_generated.emit(self._build_qrcode_pixmap(self.qrcode_url))
 
         params = {
             "source": "main-fe-header",
