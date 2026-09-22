46dc5f77bb2e9f80a75329397aa32072f289b9ad
ScottSloan <world1019@sina.com>
2026-09-22 17:53:54 +0800

    🐛 fix: 登录态初始化的网络回调改为绑定方法，避免闭包直连引发主线程死锁
    UserManager、CookieManager 的 success/error 回调之前是局部闭包，
    Qt 无法识别其接收线程，连接退化为直连，导致回调就地跑在网络请求的
    子线程里。改为连到 QObject 的绑定方法后，Qt 能正确解析成排队连接，
    回调统一切回主线程执行。

--- src/util/auth/user.py
+++ src/util/auth/user.py
@@ -1,3 +1,5 @@
+from PySide6.QtCore import QObject, Slot
+
 from ..common.signal_bus import signal_bus
 from ..common.translator import Translator
 from ..common.config import config
@@ -12,86 +14,81 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-class UserManager(AuthBase):
+class UserManager(AuthBase, QObject):
     def __init__(self):
-        super().__init__()
+        AuthBase.__init__(self)
+        QObject.__init__(self)
 
     def init_user_info(self):
         self.get_user_info()
 
     def get_user_info(self):
-        def on_success(response: dict):
-            data: dict = response.get("data", {})
-
-            img_url = data["wbi_img"]["img_url"]
-            sub_url = data["wbi_img"]["sub_url"]
-
-            config.set(config.img_key, Path(img_url).stem, save = False)
-            config.set(config.sub_key, Path(sub_url).stem, save = False)
-
-            if data.get("isLogin"):
-                runtime.auth.uname = data.get("uname", "")
-                runtime.auth.uid = data.get("mid")
-
-                self.get_user_avatar(data.get("face", ""))
-
-                signal_bus.emit_signal(signal_bus.parse.update_preview_info)
-
-                logger.info("用户信息获取成功，用户名: %s, UID: %s", runtime.auth.uname, runtime.auth.uid)
-
-            else:
-                if config.get(config.is_login):
-                    runtime.auth.is_expired = True
-
-                    self.show_toast_error(
-                        Translator.ERROR_MESSAGES("LOGIN_EXPIRED"),
-                        Translator.ERROR_MESSAGES("LOGIN_EXPIRED_MESSAGE")
-                    )
-
-                    return
-                
-                logger.warning("用户未登录，无法获取用户信息")
-
-        def on_error(error_message: str):
-            self.show_toast_error(Translator.ERROR_MESSAGES("USER_INFO_FAILED"), error_message)
-        
         url = "https://api.bilibili.com/x/web-interface/nav"
 
         worker = NetworkRequestWorker(url)
-        worker.success.connect(on_success)
-        worker.error.connect(on_error)
+        # 连到本对象的方法而非闭包，Qt 会把回调排队回 GUI 线程；
+        # 闭包没有可识别的接收者线程，连接会退化成直连，就地跑在网络请求的子线程里
+        worker.success.connect(self.on_get_user_info_success)
+        worker.error.connect(self.on_get_user_info_error)
 
         AsyncTask.run(worker)
+
+    @Slot(object)
+    def on_get_user_info_success(self, response: dict):
+        data: dict = response.get("data", {})
+
+        img_url = data["wbi_img"]["img_url"]
+        sub_url = data["wbi_img"]["sub_url"]
+
+        config.set(config.img_key, Path(img_url).stem, save = False)
+        config.set(config.sub_key, Path(sub_url).stem, save = False)
+
+        if data.get("isLogin"):
+            runtime.auth.uname = data.get("uname", "")
+            runtime.auth.uid = data.get("mid")
+
+            self.get_user_avatar(data.get("face", ""))
+
+            signal_bus.emit_signal(signal_bus.parse.update_preview_info)
+
+            logger.info("用户信息获取成功，用户名: %s, UID: %s", runtime.auth.uname, runtime.auth.uid)
+
+        else:
+            if config.get(config.is_login):
+                runtime.auth.is_expired = True
+
+                self.show_toast_error(
+                    Translator.ERROR_MESSAGES("LOGIN_EXPIRED"),
+                    Translator.ERROR_MESSAGES("LOGIN_EXPIRED_MESSAGE")
+                )
+
+                return
+
+            logger.warning("用户未登录，无法获取用户信息")
+
+    @Slot(str)
+    def on_get_user_info_error(self, error_message: str):
+        self.show_toast_error(Translator.ERROR_MESSAGES("USER_INFO_FAILED"), error_message)
 
     def get_user_avatar(self, face_url: str):
         if not face_url:
             return
 
-        def on_success(response: bytes):
-            signal_bus.emit_signal(signal_bus.login.update_avatar, response)
-
-        def on_error(error_message: str):
-            self.show_toast_error(Translator.ERROR_MESSAGES("USER_AVATAR_FAILED"), error_message)
-
         request = NetworkRequestWorker(face_url, response_type = ResponseType.BYTES)
-        request.success.connect(on_success)
-        request.error.connect(on_error)
+        request.success.connect(self.on_get_user_avatar_success)
+        request.error.connect(self.on_get_user_avatar_error)
 
         AsyncTask.run(request)
 
+    @Slot(object)
+    def on_get_user_avatar_success(self, response: bytes):
+        signal_bus.emit_signal(signal_bus.login.update_avatar, response)
+
+    @Slot(str)
+    def on_get_user_avatar_error(self, error_message: str):
+        self.show_toast_error(Translator.ERROR_MESSAGES("USER_AVATAR_FAILED"), error_message)
+
     def logout(self):
-        def on_success(response: dict):
-            config.set(config.is_login, False)
-            runtime.auth.is_expired = False
-
-            config.set(config.bili_jct, "")
-            config.set(config.DedeUserID, "")
-            config.set(config.DedeUserID__ckMd5, "")
-            config.set(config.SESSDATA, "")
-
-        def on_error(error_message: str):
-            self.show_toast_error(Translator.ERROR_MESSAGES("LOGOUT_FAILED"), error_message)
-
         params = {
             "biliCSRF": config.get(config.bili_jct)
         }
@@ -99,10 +96,24 @@ class UserManager(AuthBase):
         url = "https://passport.bilibili.com/login/exit/v2"
 
         worker = NetworkRequestWorker(url, request_type = RequestType.POST, params = params)
-        worker.success.connect(on_success)
-        worker.error.connect(on_error)
+        worker.success.connect(self.on_logout_success)
+        worker.error.connect(self.on_logout_error)
 
         AsyncTask.run(worker)
+
+    @Slot(object)
+    def on_logout_success(self, response: dict):
+        config.set(config.is_login, False)
+        runtime.auth.is_expired = False
+
+        config.set(config.bili_jct, "")
+        config.set(config.DedeUserID, "")
+        config.set(config.DedeUserID__ckMd5, "")
+        config.set(config.SESSDATA, "")
+
+    @Slot(str)
+    def on_logout_error(self, error_message: str):
+        self.show_toast_error(Translator.ERROR_MESSAGES("LOGOUT_FAILED"), error_message)
 
     def check_response(self, response: dict):
         if response.get("code", -1) != 0:
